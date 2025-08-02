4349accdbfa33aa6a6066c4da76c2e99f605ebcd
izzy <xiong.j.isabella@outlook.com>
2025-08-02 22:49:29 +0800

    finish Basic login/signup

---
+++ application.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
+
+# Simulated database
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            flash('Logged in successfully!')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid credentials')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            flash('Username already exists!')
+        else:
+            users[username] = password
+            flash('Account created! Please log in.')
+            return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out!')
+    return redirect(url_for('login'))
