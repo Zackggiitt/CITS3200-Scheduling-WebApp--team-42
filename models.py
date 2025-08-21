063319b32caab4570b6b5df01ede61edc6da7e56
izzy <xiong.j.isabella@outlook.com>
2025-08-21 21:27:51 +0800

    added a notification model

--- models.py
+++ models.py
@@ -160,3 +160,16 @@ class FacilitatorSkill(db.Model):
     
     def __repr__(self):
         return f'<FacilitatorSkill {self.facilitator.email} - {self.module.module_name} ({self.skill_level.value})>'
+    
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(255), nullable=False)
+    is_read = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='notifications')
+
+    def __repr__(self):
+        return f'<Notification {self.user.email} - {self.message[:20]}>'
+
